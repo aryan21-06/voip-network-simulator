47324fd9ad212ef60e2a418b7c872c2f901112e4
aryan21-06 <aryansewani0@gmail.com>
2025-09-13 19:12:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/voip-network-simulator/',  // important for GitHub Pages
+  plugins: [react()]
 })
